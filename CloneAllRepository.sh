7cf78d54f7856bf1668db3b21ee5c226ede44167
mrdenchik <trasherrr@mail.ru>
2018-10-23 16:31:52 +0300

    Add CloneAllRepository.sh

--- CloneAllRepository.sh
+++ CloneAllRepository.sh
@@ -1,5 +1,29 @@
-#/bin/sh
-curl -s  https:/api.github.com/users/mrdenchik/repos?page=1 | grep -e 'git_url*' | cut -d " -f 4 | xargs -L1 git clone
+#/bin/bash
+
+if [ -z "$1" ]; then
+ echo "No username specified"
+ exit 1
+else
+ username=$1
+fi
+
+if [ -z "$2" ]; then
+ max_page=2
+else
+ max_page=$2
+fi
+
+cntx="users"
+page=1
+
+until(($page -lt $max_page))
+do
+ curl -s https:/api.github.com/$cntx/$username/repos?page=$page | grep -e 'git_url*' | cut -d " -f 4 | xargs -L1 git clone
+ $page=$page+1
+done
+
+exit 0
+
 
 #git remote set-url origin https://github.com/mrdenchik/projectname.git
 

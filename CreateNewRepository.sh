e51642e365f2c5640be0d55ea6bbb7d88797b297
mrdenchik <trasherrr@mail.ru>
2018-10-23 14:32:11 +0300

    Create repository

---
+++ CreateNewRepository.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+curl -u 'mrdenchik' https://api.github.com/user/repos -d '{"name":"projectname","description":"This project is a test"}'
+echo "# projectname" >> README.md
+git init
+git add .
+git commit -m "Create repository"
+git remote add origin https://github.com/mrdenchik/projectname.git
+git push -u origin master

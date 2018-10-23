6180399e53b985455ad3d9d96c60b70ef22f56fc
mrdenchik <trasherrr@mail.ru>
2018-10-23 15:52:02 +0300

    Add command set-url

--- CloneAllRepository.sh
+++ CloneAllRepository.sh
@@ -1,2 +1,5 @@
 #/bin/sh
 curl -s  https:/api.github.com/users/mrdenchik/repos?page=1 | grep -e 'git_url*' | cut -d " -f 4 | xargs -L1 git clone
+
+#git remote set-url origin https://github.com/mrdenchik/projectname.git
+

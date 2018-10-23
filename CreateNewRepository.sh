821c73c9f5ee68526995b7824e2a93ece87f4414
mrdenchik <trasherrr@mail.ru>
2018-10-23 14:36:07 +0300

    Add create folder

--- CreateNewRepository.sh
+++ CreateNewRepository.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 curl -u 'mrdenchik' https://api.github.com/user/repos -d '{"name":"projectname","description":"This project is a test"}'
+mkdir projectname
+cd projectname
 echo "# projectname" >> README.md
 git init
 git add .

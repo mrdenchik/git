2f4f737db17a0901a10a4dcccf87119907657271
mrdenchik <trasherrr@mail.ru>
2018-10-31 14:00:37 +0300

    Change username

--- CreateNewRepository.sh
+++ CreateNewRepository.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -u 'mrdenchik' https://api.github.com/user/repos -d '{"name":"projectname","description":"This project is a test"}'
+curl -u 'gituser' https://api.github.com/user/repos -d '{"name":"projectname","description":"This project is a test"}'
 mkdir projectname
 cd projectname
 echo "# projectname" >> README.md

aa862f51233fb85a3c72cd371bc339ccf80c35ab
sadmin <sadmin@mowspsql01.absgroup.ru>
2018-11-09 15:57:33 +0300

    change username, projectname

--- CreateNewRepository.sh
+++ CreateNewRepository.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-curl -u 'gituser' https://api.github.com/user/repos -d '{"name":"projectname","description":"This project is a test"}'
-mkdir projectname
-cd projectname
-echo "# projectname" >> README.md
+curl -u 'gituser' https://api.github.com/user/repos -d '{"name":"<projectname>","description":"This project is a test"}'
+mkdir <projectname>
+cd <projectname>
+echo "# <projectname>" >> README.md
 git init
 git add .
 git commit -m "Create repository"
-git remote add origin https://github.com/mrdenchik/projectname.git
+git remote add origin https://github.com/<gituser>/<projectname>.git
 git push -u origin master
